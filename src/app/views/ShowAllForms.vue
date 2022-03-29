ba3b9383da45ac52db9f6b10b78c2047be95e969
Amit Pratap Singh <samit5585@gmail.com>
2022-03-29 23:22:02 +0530

    some changes

--- src/app/views/ShowAllForms.vue
+++ src/app/views/ShowAllForms.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="showAllForms">
       <button @click="closePage">close</button>
+      <form @submit.prevent="submitData">
     <div v-for="item of items" :key="item.id">
       <!-- {{item.name}} -->
       <div v-if="item.name == 'name'">
@@ -19,15 +20,19 @@
               placeholder="First Name"
               id="fname"
               type="text"
-              readonly
+              required
+              @blur="nameInputValidation"
+               
             />
+            <p v-if="useNameValidity=='invalid'">Input Error</p>
             <input
               type="text"
               name="last name"
               id="lname"
               placeholder="Last Name"
-              readonly
+              
               class="cursorPointerStyle"
+              
             />
           </div>
         </div>
@@ -35,17 +40,56 @@
       <div v-else-if="item.name == 'heading'">Heading</div>
       <div v-else-if="item.name == 'Email Address'">email</div>
       <div v-else>Number</div>
+
     </div>
+    <button type="submit">Sumbit</button>
+    </form>
   </div>
 </template>
 <script>
 export default {
+  data(){
+    return{
+useNameValidity:''
+    }
+  },
   props: ["items"],
 methods: {
     closePage(){
         this.$emit('close-page')
         console.log('close');
+    },
+    submitData(){
+      
+      console.log('submit');
+      
+    },
+    nameInputValidation(e){
+      // if(this.items.isRequired==true){
+        
+      // }
+      for (const item of this.items) {
+        if(item){
+          item.isRequired=true
+          if(item.isRequired==true){
+
+            if(e.target.value.trim()===''){
+              this.useNameValidity='invalid'
+              console.log(item.isRequired);
+            }
+            else{
+              this.useNameValidity='valid'
+            }
+          }
+      else{
+        console.log("do");
+      }
+        }
+        
+      }
+       
     }
 },
+
 };
 </script>
